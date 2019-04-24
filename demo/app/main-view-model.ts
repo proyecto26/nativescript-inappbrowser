1e19b7554ade63d67efbad786518f26cabfce8d0
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2019-04-24 00:37:58 -0500

    Update demo

--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,20 +1,44 @@
-import { Observable } from 'tns-core-modules/data/observable';
+import { Observable } from 'tns-core-modules/data/observable'
 import { alert } from "tns-core-modules/ui/dialogs"
-import InAppBrowser from 'nativescript-inappbrowser';
+import InAppBrowser from 'nativescript-inappbrowser'
 
 export class HelloWorldModel extends Observable {
-  private _url: string;
+  private _url: string
 
   constructor() {
-    super();
+    super()
 
     // Initialize default values.
-    this._url = 'https://www.google.com';
+    this._url = 'https://www.google.com'
   }
 
   openLink = async () => {
     try{
-      const response = await InAppBrowser.open(this.url)
+      const response = await InAppBrowser.open(this.url, {
+        // iOS Properties
+        dismissButtonStyle: 'cancel',
+        preferredBarTintColor: 'gray',
+        preferredControlTintColor: 'white',
+        readerMode: false,
+        // Android Properties
+        showTitle: true,
+        toolbarColor: '#6200EE',
+        secondaryToolbarColor: 'black',
+        enableUrlBarHiding: true,
+        enableDefaultShare: true,
+        forceCloseOnRedirection: false,
+        // Specify full animation resource identifier(package:anim/name)
+        // or only resource name(in case of animation bundled with app).
+        animations: {
+          startEnter: 'slide_in_right',
+          startExit: 'slide_out_left',
+          endEnter: 'slide_in_right',
+          endExit: 'slide_out_left',
+        },
+        headers: {
+          'my-custom-header': 'my custom header value'
+        }
+      })
       alert({
         title: 'Response',
         message: JSON.stringify(response),
@@ -31,13 +55,13 @@ export class HelloWorldModel extends Observable {
   }
 
   get url(): string {
-    return this._url;
+    return this._url
   }
 
   set url(value: string) {
     if (this._url !== value) {
-      this._url = value;
-      this.notifyPropertyChange("url", value);
+      this._url = value
+      this.notifyPropertyChange("url", value)
     }
   }
 }
