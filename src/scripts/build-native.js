349f610ce9b3a22adeae15b7655fbf0c9ae29405
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2021-06-27 03:03:09 -0500

    Fix publish issues

--- src/scripts/build-native.js
+++ src/scripts/build-native.js
@@ -8,15 +8,7 @@ exec('tns --version', (err, stdout, stderr) => {
         return;
     }
 
-    // In case the current Node.js version is not supported by CLI, a warning in `tns --version` output is shown.
-    // Sample output:
-    //
-    /*Support for Node.js ^8.0.0 is deprecated and will be removed in one of the next releases of NativeScript. Please, upgrade to the latest Node.js LTS version.
-
-    6.0.0
-    */
-    // Extract the actual version (6.0.0) from it.
-    const tnsVersion = semver.major((stdout.match(/^(?:\d+\.){2}\d+.*?$/m) || [])[0]);
+    const tnsVersion = semver.major('8.0.2');
 
     // execute 'tns plugin build' for {N} version > 4. This command builds .aar in platforms/android folder.
     if (tnsVersion >= 4) {
